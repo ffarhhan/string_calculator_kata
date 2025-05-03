72433288a1f1b98bec391db3ff707fdfa7face59
Mohammad Farhan <farhan.mohammad21@gmail.com>
2025-05-03 18:27:13 +0530

    feat: support custom delimiters using //delimiter\n format

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,15 @@
 class StringCalculator
   def self.add(input)
     return 0 if input == ""
-    numbers = input.split(/[\n,]/).map(&:to_i)
+
+    if input.start_with?("//")
+      delimiter, input = input.split("\n", 2)
+      delimiter = delimiter[2..]
+      numbers = input.split(/[\n,#{Regexp.escape(delimiter)}]/).map(&:to_i)
+    else
+      numbers = input.split(/[\n,]/).map(&:to_i)
+    end
+
     numbers.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,5 +14,8 @@ RSpec.describe StringCalculator do
     it "handles newline as valid delimiter" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+    it "supports custom delimiters using // syntax" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
