d1415db5b6e87cef5015032e55425bf0e02f5612
Mohammad Farhan <farhan.mohammad21@gmail.com>
2025-05-03 17:55:24 +0530

    test: support addition of multiple comma-separated numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,12 +1,15 @@
 require_relative '../lib/string_calculator'
- 
- RSpec.describe StringCalculator do
-   describe ".add" do
-     it "returns 0 for an empty string" do
-       expect(StringCalculator.add("")).to eq(0)
-     end
-     it "returns sum of two comma-separated numbers" do
+
+RSpec.describe StringCalculator do
+  describe ".add" do
+    it "returns 0 for an empty string" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+    it "returns sum of two comma-separated numbers" do
       expect(StringCalculator.add("1,2")).to eq(3)
     end
-   end
- end
+    it "returns sum of multiple comma-separated numbers" do
+      expect(StringCalculator.add("1,2,3,4")).to eq(10)
+    end
+  end
+end
